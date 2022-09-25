1ab44031dd5c87306dda88d7c4764add47f08488
Jayesh 10 <jayeshjvs51@gmail.com>
2022-09-26 01:55:33 +0530

    Commit

--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: '2i8r6khb',
+  projectId: 'r7p96ks8',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
